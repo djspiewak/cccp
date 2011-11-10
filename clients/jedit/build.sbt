66543db44956c10ba0f2b195c2968af20e3b5382
Daniel Spiewak <djspiewak@gmail.com>
2011-11-10 09:07:44 -0500

    Basic plugin setup

--- clients/jedit/build.sbt
+++ clients/jedit/build.sbt
@@ -1,1 +1,5 @@
 name := "cccp-jedit-client"
+
+unmanagedJars in Compile += {
+  Attributed.blank(new File("/Applications/jEdit.app/Contents/Resources/Java/jedit.jar"))
+}
---
+++ clients/jedit/src/main/scala/com/codecommit/cccp/jedit/CCCPPlugin.scala
@@ -0,0 +1,20 @@
+package com.codecommit.cccp
+package jedit
+
+import org.gjt.sp.jedit.{jEdit => JEdit, EditPlugin, View}
+
+class CCCPPlugin extends EditPlugin {
+  def start() {
+    
+  }
+  
+  def stop() {
+    
+  }
+}
+
+object CCCPPlugin {
+  def link(view: View) {
+    
+  }
+}
