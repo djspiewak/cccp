c9716e4b3ef25956d4bb3659c7f9db5cd2449cf2
Daniel Spiewak <djspiewak@gmail.com>
2011-11-11 10:07:59 -0500

    Fallback and exponential backoff for network failure

--- agent/src/main/scala/com/codecommit/cccp/agent/ServerChannel.scala
+++ agent/src/main/scala/com/codecommit/cccp/agent/ServerChannel.scala
@@ -1,14 +1,18 @@
 package com.codecommit.cccp
 package agent
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ActorRef, Scheduler}
 
 import blueeyes.core.service.engines.HttpClientXLightWeb
 import blueeyes.core.http.MimeTypes._
 
+import java.util.concurrent.TimeUnit
+
 class ServerChannel(protocol: String, host: String, port: Int) extends Actor {
   import ServerChannel._
   import server.OpChunkUtil._
+  
+  val MaxDelay = 30 * 1000       // 30 seconds
   
   val client = {
     val back = new HttpClientXLightWeb
@@ -16,11 +20,24 @@ class ServerChannel(protocol: String, host: String, port: Int) extends Actor {
   }
   
   def receive = {
-    case PerformEdit(id, op) =>
-      client.post("/" + id + "/")(opToChunk(Vector(op)))
+    case PerformEdit(id, op, delay) => {
+      client.post("/" + id + "/")(opToChunk(Vector(op))) ifCanceled { _ =>
+        val delay2 = math.max(delay * 2, MaxDelay)
+        val randomDelay = (math.random * delay).toInt
+        Scheduler.scheduleOnce(self, PerformEdit(id, op, delay2), randomDelay, TimeUnit.MILLISECONDS)
+      }
+    }
     
-    case Poll(id, version, callback) => {
-      for (resp <- client.get("/" + id + "/" + version); content <- resp.content) {
+    case Poll(id, version, callback, delay) => {
+      val pollResults = client.get("/" + id + "/" + version)
+      
+      pollResults ifCanceled { _ =>
+        val delay2 = math.max(delay * 2, MaxDelay)
+        val randomDelay = (math.random * delay).toInt
+        Scheduler.scheduleOnce(self, Poll(id, version, callback, delay2), randomDelay, TimeUnit.MILLISECONDS)
+      }
+      
+      for (resp <- pollResults; content <- resp.content) {
         if (content.data.isEmpty) {
           self ! Poll(id, version, callback)
         } else {
@@ -33,8 +50,8 @@ class ServerChannel(protocol: String, host: String, port: Int) extends Actor {
 }
 
 object ServerChannel {
-  case class PerformEdit(id: String, op: Op)
+  case class PerformEdit(id: String, op: Op, delay: Int = 1)
   case class EditsPerformed(id: String, op: Seq[Op])
   
-  case class Poll(id: String, version: Int, callback: ActorRef)
+  case class Poll(id: String, version: Int, callback: ActorRef, delay: Int = 1)
 }
